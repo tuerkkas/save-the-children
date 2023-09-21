63ad1e0cdde35e1992f2abc33e0c82a774758bdd
Jorge Diaz <tuerkas@gmail.com>
2023-09-21 11:08:35 +0100

    Initial update
    Initial update Spring boot generated

---
+++ src/main/java/com/savethechildren/users/UsersContainerApplication.java
@@ -0,0 +1,13 @@
+package com.savethechildren.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersContainerApplication.class, args);
+	}
+
+}
